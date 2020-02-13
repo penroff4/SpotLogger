13a9c34b21ba8548b8e9044c9fb0356da9bc89ce
penroff4 <penroff4@gmail.com>
2020-02-12 20:38:25 -0800

    added data folder added Models folder added misc folder added renamed test.py runner.py

--- tangents/set-and-call/list-variables.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# for i in os.environ:
-#   print(i)
-
-spotipy_var = ['SPOTIPY_CLIENT_ID', 'SPOTIPY_CLIENT_SECRET', 'SPOTIPY_REDIRECT_URI']
-
-for i in spotipy_var:
-    print(os.environ[i])
