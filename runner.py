f488c4c19b363539be688ca2149b32abc884c919
penrofff4 <penroff4@yahoo.com>
2020-02-15 23:53:03 -0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,20 +1,25 @@
 import os
+import json
 from Models.SpotLogger import SpotLogger
+from Models.ScribeLogger import ScribeLogger
 from Models.LiteLogger import LiteLogger
 
 # Spotify API Connect details
-SPOTIPY_CLIENT_ID = None
-SPOTIPY_CLIENT_SECRET = None
+SPOTIPY_CLIENT_ID = '790fb465c980470886e0887c7c02cb7a'
+SPOTIPY_CLIENT_SECRET = 'a41fec8061c54bfd94c1d857c2965ba5'
 SPOTIPY_REDIRECT_URI = 'http://google.com'
 SPOTIPY_USERNAME = 'penroff4@gmail.com'
 SPOTIPY_SCOPE = 'user-read-recently-played'
+
+# JSON Output file
+JSON_output_file = 'recently-played-list.txt'
 
 # sqlite variables
 SPOTLOGGER_DB = os.getcwd()+"\data\SpotLogger.db"
 COMMIT_CONFIRM = None
 
 # Create SpotLogger to call Spotify
-SpotLoggerOne = SpotLogger.SpotLogger(
+SpotLoggerOne = SpotLogger(
     SPOTIPY_USERNAME
     , SPOTIPY_SCOPE
     , SPOTIPY_CLIENT_ID
@@ -22,56 +27,72 @@ SpotLoggerOne = SpotLogger.SpotLogger(
     , SPOTIPY_REDIRECT_URI
 )
 
+# Create ScribeLogger to prep data into records
+ScribeLoggerOne = ScribeLogger(
+    JSON_output_file
+)
+
 # Create LiteLogger to log results to sqlite
-LiteLoggerOne = LiteLogger.LiteLogger(
+LiteLoggerOne = LiteLogger(
     SPOTLOGGER_DB
 )
 
-###_MAIN_###
+if __name__ == "__main__":
 
-#  prompts user to authorize app
-SpotLoggerOne.get_spotify_auth_token()
+    #  prompts user to authorize app
+    SpotLoggerOne.get_spotify_auth_token()
 
-#  gets json of recently played tracks from spotify
-SpotLoggerOne.get_recently_played_songs()
+    #  gets json of recently played tracks from spotify
+    SpotLoggerOne.get_recently_played_songs()
 
-#  outputs json to file
-SpotLoggerOne.output_recently_played_to_json_file()
+    #  takes dump_file, outputs JSON to dump_file
+    SpotLoggerOne.output_recently_played_to_json_file(JSON_output_file)
 
-# operate over JSON to build payload table
-# payload_data = []
+    # read dump_file back in as JSON, self.rpl_json
+    ScribeLoggerOne.read_json()
+    
+    # build payload_df
+    ScribeLoggerOne.prep_payload_data()
 
-# operate over JSON to build context table
-# context_data = []
+    # build self.payload_data
+    ScribeLoggerOne.prep_payload_data()
 
-# operate over JSON to build track table
-# track_data = []
+    # build items_df
+    ScribeLoggerOne.prep_items_df()
 
-# operate over JSON to build artist table
-# artist_data = []
+    # payload_data = []
 
-# operate over JSON to build album table
-# album_data = []
+    # operate over JSON to build context table
+    # context_data = []
 
-# Connect to sqlitedb
-LiteLoggerOne.setup()
+    # operate over JSON to build track table
+    # track_data = []
 
-# insert payload record(s)
-LiteLoggerOne.record_payload_sqllite(payload_data)
+    # operate over JSON to build artist table
+    # artist_data = []
 
-# insert context record(s)
-LiteLoggerOne.record_context_sqllite(context_data)
+    # operate over JSON to build album table
+    # album_data = []
 
-# insert track record(s)
-LiteLoggerOne.record_track_sqllite(track_data)
+    # Connect to sqlitedb
+    # LiteLoggerOne.setup()
 
-# insert artist record(s)
-LiteLoggerOne.record_artist_sqllite(artist_data)
+    # insert payload record(s)
+    # LiteLoggerOne.record_payload_sqllite(payload_data)
 
-# insert album record(s)
-LiteLoggerOne.record_album_sqllite(album_data)
+    # insert context record(s)
+    # LiteLoggerOne.record_context_sqllite(context_data)
 
-if true:
-    LiteLoggerOne.teardown(COMMIT_CONFIRM)
-else:
-    return false
+    # insert track record(s)
+    # LiteLoggerOne.record_track_sqllite(track_data)
+
+    # insert artist record(s)
+    # LiteLoggerOne.record_artist_sqllite(artist_data)
+
+    # insert album record(s)
+    # LiteLoggerOne.record_album_sqllite(album_data)
+
+    # if 1 == 1:
+    #    LiteLoggerOne.teardown(COMMIT_CONFIRM)
+    # else:
+    #    0
